9ae25e13820802ce645164ddbb692909760a86ed
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-08-22 23:11:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const button = document.querySelectorAll(".btn");
+const stopbutton = document.querySelector(".stop");
+
+button.forEach(buttons=>{
+	buttons.addEventListner('click',()=>{
+		if(currentSound){
+			currentSound.pause();
+			currentSound.currentTime=0;
+		}
+		const soundFile = buttons.getAttribute('data-sound');
+		currentSound=new Audio('sounds/${soundFile}');
+		currentSound.play();
+	})
+})
+stopbutton.addEventListner('click', ()=>{
+	if(currentSound){
+		currentSound.pause();
+		currentSound.currentTime=0;
+	}
+})
+
+buttons.addEventListener("click" , ()=>{
+    	console.log(button);
+})
