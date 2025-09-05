360a853e4adab81c3d601339c5c819db6063d62e
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-09-05 12:00:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,27 +3,26 @@ let currentSound = null;
 const buttons = document.querySelectorAll(".btn");
 const stopButton = document.querySelector(".stop");
 
+// Play sound on button click
 buttons.forEach(btn => {
-	btn.addEventListener('click', () => {
-		// Stop current sound if playing
-		if(currentSound){
-			currentSound.pause();
-			currentSound.currentTime = 0;
-		}
+  btn.addEventListener('click', () => {
+    // Stop current sound if playing
+    if(currentSound){
+      currentSound.pause();
+      currentSound.currentTime = 0;
+    }
 
-		// Get sound file from data attribute
-		const soundFile = btn.getAttribute('data-sound');
-
-		// Play new sound
-		currentSound = new Audio(`sounds/${soundFile}`);
-		currentSound.play();
-	});
+    // Play new sound
+    const soundFile = btn.getAttribute('data-sound');
+    currentSound = new Audio(`sounds/${soundFile}`);
+    currentSound.play();
+  });
 });
 
 // Stop button functionality
 stopButton.addEventListener('click', () => {
-	if(currentSound){
-		currentSound.pause();
-		currentSound.currentTime = 0;
-	}
+  if(currentSound){
+    currentSound.pause();
+    currentSound.currentTime = 0;
+  }
 });
